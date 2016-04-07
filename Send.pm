300d119acbf3b9c01f054ac101179c391bcf4d05
Michal Špaček <m.spacek@ibsmm.com>
2016-04-07 11:39:11 +0200

    Prepare for 0.02 version.

--- Send.pm
+++ Send.pm
@@ -13,7 +13,7 @@ use Net::HL7::Message;
 use Perl6::Slurp qw(slurp);
 
 # Version.
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constructor.
 sub new {
@@ -189,6 +189,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
