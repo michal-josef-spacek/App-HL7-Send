fa01d1c9bc467c6ff88551974df66a064f93e85f
Michal Josef Špaček <tupinek@gmail.com>
2017-10-18 17:07:19 +0200

    Update author name.

--- Send.pm
+++ Send.pm
@@ -185,13 +185,13 @@ L<https://github.com/tupinek/App-HL7-Send>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2016-2017 Michal Špaček
+ © 2016-2017 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
