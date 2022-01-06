84c5ceb1f72d01b458bebad57ba3f6dea986f5b8
Michal Josef Špaček <tupinek@gmail.com>
2022-01-06 15:06:33 +0100

    Prepare for 0.04 version

--- Send.pm
+++ Send.pm
@@ -10,7 +10,7 @@ use Net::HL7::Connection;
 use Net::HL7::Message;
 use Perl6::Slurp qw(slurp);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Constructor.
 sub new {
@@ -192,6 +192,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
